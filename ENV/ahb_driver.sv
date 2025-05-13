69be37613abebb7def62345d5a5a4024dd1c9c11
HackAnshul <91744882+HackAnshul@users.noreply.github.com>
2025-05-13 15:06:14 +0530

    Update ahb_driver.sv

--- ENV/ahb_driver.sv
+++ ENV/ahb_driver.sv
@@ -11,10 +11,9 @@
 `define AHB_DRIVER_SV
 
 class ahb_driver;
-  ahb_trans trans_h, trans_h2;
-  ahb_trans addr_que[$];
-  ahb_trans data_que[$];
-
+  ahb_trans trans_h, trans_h2, trans_h3;
+  ahb_trans addr_phase_que[$];
+  ahb_trans data_phase_que[$];
 
   //mailbox
   mailbox #(ahb_trans) gen2drv;
@@ -29,38 +28,51 @@ class ahb_driver;
     this.vif = vif;
   endfunction
 
-  task run();
-    wait_reset_release();
+  //description
+  task drive_control_io(); //drives control signals of AHB
+    bit first_trans_flag = 1'b0; //flag for indicating the first operation of the burst
+
     forever begin
-      trans_h = new();
-      fork
-        begin
-          gen2drv.get(trans_h);
-          addr_que.push_back(trans_h);
-          // trans_h.print(trans_h,"Driver");
-          // send_to_dut();
+      wait(addr_phase_que != 0)
+      trans_h2=addr_phase_que.pop_front();
+      for(int i =0; i<trans_h2.calc_txf; i++) begin
+        if(i==0) begin
+          if(!first_trans_flag) @(vif.drv_cb);
+          first_trans_flag = 1'b1;
+          vif.drv_cb.hwrite <= trans_h2.hwrite;
+          vif.drv_cb.hsize <= trans_h2.hsize;
+          vif.drv_cb.hburst <= trans_h2.hburst;
+          vif.drv_cb.htrans <= 2'h10;               //NONSEQ
+          data_phase_que.push_back(trans_h2);
         end
-        drive_control();
-        drive_data();
-        // -> drv_comp;
-        wait_reset_assert();
-      join_any
-      disable fork;
-      wait_reset_release();
+
+        if(i!=0)
+          vif.drv_cb.htrans <= 2'h11;               //SEQ
+
+        //always sends address
+        vif.drv_cb.haddr <= trans_h2.haddr_que.pop_front();
+        @(vif.drv_cb iff vif.drv_cb.hreadyout);
+      end
     end
   endtask
+   task drive_data_in();                               //drives the data signals
+    bit first_trans_flag = 1'b0;
 
-  //description
-  task drive_control();
-    wait(addr_que != 0)
-    trans_h2 = addr_que.pop_front();
-    
-  endtask
+    forever begin
+      wait(data_phase_que != 0);
 
-  task send_to_dut();
-    //drive data to design
-    @(vif.drv_cb);
-//driver logic
+      trans_h3 = data_phase_que.pop_front();
+      if(!first_trans_flag)
+        @(vif.drv_cb);
+
+      first_trans_flag = 1'b1;
+
+      for(int i=0; i<trans_h3.calc_txf;i++) begin
+        if(trans_h3.HWRITE)
+          vif.drv_cb.hwdata <= trans_h3.hwdata;
+        @(vif.drv_cb iff vif.drv_cb.hreadyout);
+      end
+    end
   endtask
 
   task wait_reset_release();
@@ -68,6 +80,21 @@ class ahb_driver;
   endtask
   task wait_reset_assert();
     wait (vif.drv_cb.HRESETn == 0);
+  endtask
+  
+ task run();
+    trans_h = new();
+    trans_h2 =new();
+    trans_h3 = new();
+    fork
+      forever begin
+        gen2drv.get(trans_h);
+        trans_h.print("Driver");
+        addr_phase_que.push_back(trans_h);
+      end
+      drive_control_io();                       //drives the control signals of AHB
+      drive_data_in();                          //drives the data signal of AHB
+    join
   endtask
 endclass
 `endif
